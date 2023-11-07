1168fb4da6d8ea78417c7c8d09849ba509e50bf5
Daniel Jurčík <dano.jurcik@gmail.com>
2023-11-07 12:23:25 +0100

    Updated code structure

---
+++ web/components/modal/modal.js
@@ -0,0 +1,6 @@
+window.onload = function get_body() {
+    const bodyElem = document.querySelector("body");
+    let overlay = document.createElement("div");
+    overlay.id = 'overlay';
+    bodyElem.appendChild(overlay);
+}
